cbd29e28e01a5206d02cf79b5d4c5ee84500b9ba
ylj <ylj@ylj.com>
2018-07-19 18:36:52 +0800

    55

--- demo/src/pages/baseinfo/index.vue
+++ demo/src/pages/baseinfo/index.vue
@@ -54,57 +54,84 @@
 		                    <h4>技术模块</h4>
 		                </Col>
 		                <Col span="22" >
-		                    <Tag v-for="item in count" :key="item" :name="item" closable @on-close="handleClose2">技术模块{{ item + 1 }}</Tag>
-		                    <Button icon="ios-plus-empty" type="dashed" size="small" @click="handleAdd">添加</Button>
+		                    <Tag v-for="item in count" :key="item" :name="item" closable @on-close="handleClose">{{ item}}</Tag>
+		                    <Button icon="ios-plus-empty" type="dashed" size="small" @click="addItem">添加技术模块</Button>
 		                </Col>
 		            </Row>
-                  <!--   <Select v-model="model1" style="width:200px">
-				        <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
-				    </Select>
-				    <Button type="ghost">添加模块</Button> -->
+		            <Row class="tagBox">
+		                <Col span="2">
+		                    <h4>业务模块</h4>
+		                </Col>
+		                <Col span="22" >
+		                    <Tag v-for="item in count2" :key="item" :name="item" closable @on-close="handleClose2">{{ item}}</Tag>
+		                    <Button icon="ios-plus-empty" type="dashed" size="small" @click="addItem2">添加业务模块</Button>
+		                </Col>
+		            </Row>
+               
             	</div>
 
 				
 
             </div>
 		</Card>
+
+
+		<Modal ref="addPop" v-model="modaAdd" title="添加模块" @on-ok="submitAdd"  ok-text="提交" :loading="modal_add_loading" visible="true">
+            <Form :model="formItem" :label-width="80" >
+                <FormItem label="模块名称" v-if="technologyORbusiness">
+                    <Input v-model="formItem.technologyName" placeholder="请输入项目名称"></Input>
+                </FormItem>
+                <FormItem label="模块名称" v-else>
+                    <Input v-model="formItem.businessName" placeholder="请输入项目名称"></Input>
+                </FormItem>
+               
+            </Form>
+        </Modal> 
+
+
 	</div>
 </template>
 <script>
 export default {
 	data () {
         return {
-        	count: [0, 1, 2],
-            cityList: [
-                {
-                    value: 'New York',
-                    label: 'New York'
-                },
-                {
-                    value: 'London',
-                    label: 'London'
-                },
-                {
-                    value: 'Sydney',
-                    label: 'Sydney'
-                },
-                {
-                    value: 'Ottawa',
-                    label: 'Ottawa'
-                },
-                {
-                    value: 'Paris',
-                    label: 'Paris'
-                },
-                {
-                    value: 'Canberra',
-                    label: 'Canberra'
-                }
-            ],
-            model1: ''
+        	modaAdd: false,
+            modal_add_loading: true,
+
+            technologyORbusiness:true,
+            formItem: {
+                technologyName:"",
+                businessName:"",
+            },
+        	count: ["技术模块1", "技术模块2", "技术模块3"],
+        	count2: ["业务模块1", "业务模块2", "业务模块3"],
         }
     },
      methods: {
+     	addItem(){
+            this.modaAdd = true;
+            this.technologyORbusiness = true;
+        },
+        addItem2(){
+            this.modaAdd = true;
+            this.technologyORbusiness = false;
+        },
+        formItemReset(){
+            this.$nextTick(() => {
+                this.formItem.name = "";
+                this.formItem.technologyName = "";
+                
+            });
+        },
+        submitAdd () {
+            setTimeout(() => {
+            	this.technologyORbusiness ? this.count.push(this.formItem.technologyName) : this.count2.push(this.formItem.businessName)
+                this.modaAdd = false;
+                this.$Message.info('成功');
+                this.formItemReset();
+            },1000)
+            
+        },
         handleAdd () {
             if (this.count.length) {
                 this.count.push(this.count[this.count.length - 1] + 1);
@@ -112,9 +139,13 @@ export default {
                 this.count.push(0);
             }
         },
-        handleClose2 (event, name) {
+        handleClose (event, name) {
             const index = this.count.indexOf(name);
             this.count.splice(index, 1);
+        },
+        handleClose2 (event, name) {
+            const index = this.count2.indexOf(name);
+            this.count2.splice(index, 1);
         }
     }
 }

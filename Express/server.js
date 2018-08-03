22ec965792dd78b52df4aa31ee3a6b5c4693f72a
yin <yin@yin.com>
2018-08-03 17:30:18 +0800

    92

--- Express/server.js
+++ Express/server.js
@@ -43,6 +43,39 @@ let mockData2 = (val1,val2,val3 = 200)=>{
   })
 }
 
+
+
+let mockDataList = (val1 = 200,val2 = 1,val3 = 3)=>{
+  return Mock.mock({
+      "status": val1,
+      "message":"xxxxxxx",
+      data:{
+        "list|3-9":[ {
+          "id|+1":1 ,
+          "prj_id|+1":100,
+          "prj_name|5-8":/[a-zA-Z]/,
+          "prj_manager":"项目经理",
+          "prj_desc":"项目描述",
+          "prj_goal":"项目目标",
+          "settle_time":"2018-01-01",
+          "start_time":"2018-10-10",
+          "end_time":"2018-12-10",
+          "prj_type|0-1":0,
+          "logic_sys_id":"logic_sys_id",
+          "phycics_sys_id":"phycics_sys_id",
+          "modules":"modules",
+          "prod_id|+1":500,
+          "prod_name|5-8":/[a-zA-Z]/,
+          "__value2__page":val2,
+          "__value3__pageline":val3,
+          
+      }],
+      "total|3-9":3,
+      "per_page|3-9":3,
+      },
+  })
+}
+
 app.use(allowCrossDomain);//运用跨域的中间件
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -51,25 +84,30 @@ app.use(express.static("vConsole"))
 
 
 app.all('/json.action', function(req, res) {
-
   let resVal = mockData2(req.body.firstName,req.body.lastName,req.body.myStatus);
   console.log("req==>",req.body);
   console.log("resVal==>",resVal);
   //console.log(__dirname)
-
   //console.log("res==>",res);
   if(req.body.firstName == "yin"){
-
     res.json(mockData2(req.body.firstName,req.body.lastName,req.body.myStatus));
   }else{
     res.json(mockData());
   }
   //console.log("res==>",res); 
-	
-  
-  
   res.end()
 });
+
+
+app.all('/project/all', function(req, res) {
+  let resVal = mockDataList(req.body.myStatus,req.body.page,req.body.pageline);
+  console.log("req==>",req.body);
+  console.log("resVal==>",resVal);
+  res.json(mockDataList(req.body.myStatus,req.body.page,req.body.pageline));
+  res.end()
+});
+
+
 /**
  * 监听9090端口
  */
